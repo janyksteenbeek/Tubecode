3152972dbb8acbf2c013a78e30aef9af44a506f7
Pascal Schwientek <p.schwientek@tubeone.com>
2015-05-17 01:04:10 +0200

    typo fix

--- src/Pascal/Resources/UploadableFile.php
+++ src/Pascal/Resources/UploadableFile.php
@@ -6,7 +6,7 @@ class UploadableFile extends \SplFileInfo{
     public function __construct($file)
     {
         //TODO validate if correct video file
-        parrent::__construct($file);
+        parent::__construct($file);
     }
 
 }
