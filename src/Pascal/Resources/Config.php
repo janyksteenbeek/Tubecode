3e8246eb3d094e9c1542f8022cec8da7b2576d35
Pascal Schwientek <p.schwientek@tubeone.com>
2015-05-17 19:11:41 +0200

    added config resource

---
+++ src/Pascal/Resources/Config.php
@@ -0,0 +1,26 @@
+<?php namespace Pascal\Resources;
+
+
+class Config {
+
+
+    /**
+     * @var array
+     */
+    private $settings = [];
+
+    public function __construct(array $settings)
+    {
+
+        $this->settings = $settings;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSettings()
+    {
+        return $this->settings;
+    }
+
+}
